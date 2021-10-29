8cda8ce908dbc747d923dbae1657d2705f5f282f
Marcin <marcinkrueger@onet.eu>
2021-10-29 19:36:09 +0200

    Add eslint, prettier and stylelint

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { Button } from "./Button";
-import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import React from 'react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { Button } from './Button.jsx';
 
 export const Primary = () => <Button primary={true}>Button</Button>;
 
 export const Playground = () => {
-  const children = text("children", "Default value");
-  const primary = boolean("primary", true);
+  const children = text('children', 'Default value');
+  const primary = boolean('primary', true);
 
   return <Button primary={primary}>{children}</Button>;
 };
 
 export default {
-  title: "Button",
+  title: 'Button',
   decorators: [withKnobs],
 };
