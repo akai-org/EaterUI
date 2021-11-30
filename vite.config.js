43bc368a29a3318fda07d5178743dea85f40d08c
roertbb <robert.banaszak1997@gmail.com>
2021-11-30 17:40:07 +0100

    human readable names for css modules

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    modules: {
+      generateScopedName: (name, filename) => {
+        const basename = path.basename(filename);
+        const componentName = basename.replace(".module.scss", "");
+        return `${componentName}-${name}`;
+      },
+    },
+  },
   plugins: [react()],
 });
