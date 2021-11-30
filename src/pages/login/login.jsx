d347e5d409eed3011c03df522de3fb05dd25bde1
NataliaCichonska <natalia.cichonska1103@gmail.com>
2021-11-30 17:57:17 +0100

    added view

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,15 +2,33 @@ import React from "react";
 import { GoogleLogin } from "react-google-login";
 import { Button } from "../../components/Button/Button";
 import { handleLoginSuccess } from "../../utils/auth";
+import { Icon } from "../../components/Icon/Icon";
+import styles from "./login.module.scss";
 
 const GoogleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 function Login() {
   return (
-    <div>
-      <div>ikony</div>
+    <div className={styles.login}>
+      <div className={styles.login_icons}>
+        <Icon
+          name="pencil"
+          className={styles.ligin_icons_icon}
+          size="large"
+        ></Icon>
+        <Icon
+          name="calendar"
+          className={styles.ligin_icons_icon}
+          size="large"
+        ></Icon>
+        <Icon
+          name="hamburger"
+          className={styles.ligin_icons_icon}
+          size="large"
+        ></Icon>
+      </div>
       <h1>Witaj w Eater!</h1>
-      <p>
+      <p className={styles.login_paragraph}>
         Tu możesz zebrać swoje Przepisy i zaplanować Jadłospis, a następnie
         przygotujemy do Ciebie Listę Zakupów
       </p>
@@ -19,7 +37,13 @@ function Login() {
         onSuccess={handleLoginSuccess}
         isSignedIn={true}
         render={({ onClick, disabled }) => (
-          <Button onClick={onClick} isDisabled={disabled}>
+          <Button
+            onClick={onClick}
+            isDisabled={disabled}
+            fullwidth="true"
+            className={styles.login_button}
+          >
+            <Icon name="google" color="#fff" size="small"></Icon>
             Zaloguj się z Google
           </Button>
         )}
