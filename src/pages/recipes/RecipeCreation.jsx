33e1c7d7ec4b51a223fb3221fbfc1a6ab30bc78f
Damian Kalka <damian.kalka@netguru.com>
2021-12-21 20:47:21 +0100

    extract to separated render function

--- src/pages/recipes/RecipeCreation.jsx
+++ src/pages/recipes/RecipeCreation.jsx
@@ -10,6 +10,46 @@ import styles from "./RecipeCreation.module.scss";
 import { Button } from "../../components/Button/Button";
 import { ButtonGroup } from "../../components/ButtonGroup/ButtonGroup";
 
+const renderRecipeCreation = ({ ingredients, handleEditLinkClick }) => (
+  <form className={styles.recipeCreationForm}>
+    <Text size="h3">Dodaj przepis</Text>
+    <Text size="h4" className={styles.headline}>
+      Składniki (na 1 porcję)
+      {!ingredients.length && (
+        <Text className={styles.emptyState}>
+          Nie masz żadnych składników. Dodaj coś!
+        </Text>
+      )}
+    </Text>
+    <div>
+      {ingredients.map(({ id, name, amount, measurement }) => (
+        <Card
+          key={id}
+          primaryText={name}
+          secondaryText={`${amount} ${measurement}`}
+          rightContent={
+            <Link to="add-ingredient">
+              <Icon name="pencil" size="medium" />
+            </Link>
+          }
+          onClick={() => handleEditLinkClick(id)}
+        />
+      ))}
+    </div>
+    <Button to="add-ingredient" fullwidth className={styles.actionButton}>
+      Dodaj składnik
+    </Button>
+    <ButtonGroup className={styles.buttonGroup}>
+      <Button variant="secondary" fullwidth to="..">
+        Cofnij
+      </Button>
+      <Button type="submit" fullwidth>
+        Zapisz
+      </Button>
+    </ButtonGroup>
+  </form>
+);
+
 const RecipeCreation = () => {
   const {
     ingredients,
@@ -19,49 +59,15 @@ const RecipeCreation = () => {
     handleEditLinkClick,
   } = useIngredients();
 
-  const RecipeCreationElement = () => (
-    <form className={styles.recipeCreationForm}>
-      <Text size="h3">Dodaj przepis</Text>
-      <Text size="h4" className={styles.headline}>
-        Składniki (na 1 porcję)
-        {!ingredients.length && (
-          <Text className={styles.emptyState}>
-            Nie masz żadnych składników. Dodaj coś!
-          </Text>
-        )}
-      </Text>
-      <div>
-        {ingredients.map(({ id, name, amount, measurement }) => (
-          <Card
-            key={id}
-            primaryText={name}
-            secondaryText={`${amount} ${measurement}`}
-            rightContent={
-              <Link to="add-ingredient">
-                <Icon name="pencil" size="medium" />
-              </Link>
-            }
-            onClick={() => handleEditLinkClick(id)}
-          />
-        ))}
-      </div>
-      <Button to="add-ingredient" fullwidth className={styles.actionButton}>
-        Dodaj składnik
-      </Button>
-      <ButtonGroup className={styles.buttonGroup}>
-        <Button variant="secondary" fullwidth to="..">
-          Cofnij
-        </Button>
-        <Button type="submit" fullwidth>
-          Zapisz
-        </Button>
-      </ButtonGroup>
-    </form>
-  );
-
   return (
     <Routes>
-      <Route path="/" element={<RecipeCreationElement />} />
+      <Route
+        path="/"
+        element={renderRecipeCreation({
+          ingredients,
+          handleEditLinkClick,
+        })}
+      />
       <Route
         path="add-ingredient"
         element={
