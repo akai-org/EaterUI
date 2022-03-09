17ca3735f15301a899d5d88b05375afec14a391e
Robert Banaszak <robert.banaszak1997@gmail.com>
2022-03-09 18:49:47 +0100

    add navigation between recipe pages and introduce minor adjustments for components (#61)

--- src/pages/Recipes/RecipeDetails/RecipeDetails.jsx
+++ src/pages/Recipes/RecipeDetails/RecipeDetails.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import * as React from "react";
 import { useParams } from "react-router-dom";
+import { Button, ButtonGroup, Text } from "@/components";
 
 function RecipeDetails() {
   const params = useParams();
 
-  return <div>Details {params.id}</div>;
+  return (
+    <>
+      <Text size="h2">Details</Text>
+      <Text>id: {params.id}</Text>
+      <ButtonGroup>
+        <Button variant="secondary" fullwidth to="/recipes">
+          Cofnij
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 }
 
 export default RecipeDetails;
