613af11d15b0f29c6806110a9b686e1e15c54ab4
Marcin Krueger <marcinkrueger@gmail.com>
2022-04-11 08:55:18 +0200

    fetching data to shopping list

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -2,14 +2,49 @@ import React from "react";
 import classNames from "classnames";
 import { Button, Text, Card } from "@/components";
 import styles from "./ShoppingList.module.scss";
+import useShoppingList from "./hooks/api/useShoppingList";
+
+function getDateString(date) {
+  const month = date.toLocaleDateString("pl-PL", { month: "short" });
+  const day = date.toLocaleDateString("pl-PL", { day: "2-digit" });
+  const weekday = date.toLocaleDateString("pl-PL", { weekday: "short" });
+
+  return `${day} ${
+    month.charAt(0).toUpperCase() + month.slice(1)
+  } (${weekday.slice(0, -1)})`;
+}
 
 function ShoppingList() {
+  const state = useShoppingList();
+
+  if (state.isLoading) return <p>Loading...</p>;
+
+  if (state.isError) return <p>Error: {state.error.toString()}</p>;
+
   return (
     <>
       <div className={classNames(styles.container)}>
         <Text size="h3" className={classNames(styles.header)}>
           Listy zakupów
         </Text>
+        {state.data.map((el, id) => {
+          const startDate = new Date(el.startDate);
+          const endDate = new Date(el.endDate);
+
+          return (
+            <Card
+              primaryText={`${getDateString(startDate)} - ${getDateString(
+                endDate,
+              )}`}
+              secondaryText={
+                !el.markedIngredients
+                  ? `${el.ingredients} Produktów`
+                  : `${el.markedIngredients}/${el.ingredients} produktów zakupionych`
+              }
+              key={id}
+            />
+          );
+        })}
         <Card
           primaryText={"28 Paź (pon) - 07 Lis (wt)"}
           secondaryText={"888 Produktów"}
--- src/pages/ShoppingList/hooks/api/useShoppingList.js
+++ src/pages/ShoppingList/hooks/api/useShoppingList.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import request from "@/api/request";
 
 function useShoppingList() {
-  return useQuery("shopping-list", () => request("/recipes"));
+  return useQuery("shopping-list", () => request("/shopping-list"));
 }
 
 export default useShoppingList;
