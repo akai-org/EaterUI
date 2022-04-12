076c6568cb285dcb501efb3b85db6242e022b77b
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-04-12 13:51:27 +0200

    add RecipeDetails view

--- src/pages/Recipes/RecipeDetails/RecipeDetails.jsx
+++ src/pages/Recipes/RecipeDetails/RecipeDetails.jsx
@@ -1,14 +1,23 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
 import { Button, ButtonGroup, Text } from "@/components";
+import useRecipe from "@/pages/Recipes/hooks/api/useRecipe";
 
 function RecipeDetails() {
   const params = useParams();
+  const recipeId = params.id;
+  console.log(recipeId);
+  const tekst = `id przepisu: ${recipeId}`;
+  const recipeDetails = useRecipe(recipeId);
+  console.log(recipeDetails);
+
+  if (recipeDetails.isLoading) return <p>Loading...</p>;
+  if (recipeDetails.isError) return <p>Error: an error occured</p>;
 
   return (
     <>
-      <Text size="h2">Details</Text>
-      <Text>id: {params.id}</Text>
+      <Text size="h2">{recipeDetails.data.name}</Text>
+      <Text size="h3">{tekst}</Text>
       <ButtonGroup>
         <Button variant="secondary" fullwidth to="/recipes">
           Cofnij
