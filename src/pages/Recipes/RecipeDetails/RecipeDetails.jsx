079e65cb97559bc104aabdd15fb3832190fdca22
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-04-12 14:36:25 +0200

    style recipeDetails view

--- src/pages/Recipes/RecipeDetails/RecipeDetails.jsx
+++ src/pages/Recipes/RecipeDetails/RecipeDetails.jsx
@@ -2,25 +2,36 @@ import * as React from "react";
 import { useParams } from "react-router-dom";
 import { Button, ButtonGroup, Text } from "@/components";
 import useRecipe from "@/pages/Recipes/hooks/api/useRecipe";
+import styles from "./RecipeDetails.module.scss";
 
 function RecipeDetails() {
   const params = useParams();
   const recipeId = params.id;
-  console.log(recipeId);
-  const tekst = `id przepisu: ${recipeId}`;
   const recipeDetails = useRecipe(recipeId);
-  console.log(recipeDetails);
 
   if (recipeDetails.isLoading) return <p>Loading...</p>;
   if (recipeDetails.isError) return <p>Error: an error occured</p>;
 
   return (
     <>
-      <Text size="h2">{recipeDetails.data.name}</Text>
-      <Text size="h3">{tekst}</Text>
+      <div className={styles.recipeDetails}>
+        <Text size="h2">{recipeDetails.data.name}</Text>
+        <img source={recipeDetails.data.graphicURL} alt="dish graphic"></img>
+        <p>{recipeDetails.data.description}</p>
+        <Text size="h4">Składniki (na 1 porcję)</Text>
+        {recipeDetails.data.ingredients.map((ingredient) => (
+          <div className={styles.ingredient} key={ingredient.name}>
+            <p>{ingredient.name}</p>
+            <p>{`${ingredient.amount} ${ingredient.measure}`}</p>
+          </div>
+        ))}
+      </div>
       <ButtonGroup>
         <Button variant="secondary" fullwidth to="/recipes">
           Cofnij
+        </Button>
+        <Button variant="primary" fullwidth to={`/recipes/${recipeId}/edit`}>
+          Edytuj
         </Button>
       </ButtonGroup>
     </>
