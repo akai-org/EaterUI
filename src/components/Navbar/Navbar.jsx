8be9ec48a067e9dcb2c1cce047b0647fa63e6f24
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-05-17 21:52:40 +0200

    added darkish overlay on opened navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,50 +3,58 @@ import { Link } from "react-router-dom";
 import cn from "classnames";
 import styles from "./Navbar.module.scss";
 
+const navItems = [
+  { path: "/menu", text: "Twój jadłospis" },
+  { path: "/recipes", text: "Przepisy" },
+  { path: "/shopping-list", text: "Lista zakupów" },
+  { path: "#", text: "Twój profil" },
+];
+
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const closeNavbar = () => setIsOpen(false);
-
-  const navItems = [
-    { path: "/menu", text: "Twój jadłospis" },
-    { path: "/recipes", text: "Przepisy" },
-    { path: "/shopping-list", text: "Lista zakupów" },
-    { path: "#", text: "Twój profil" },
-  ];
+  const toggleNavbar = () => setIsOpen((prevIsOpen) => !prevIsOpen);
 
   return (
-    <nav role="navigation" aria-label="menu" className={styles.navbar}>
+    <>
       <div
-        className={cn(styles.menuList, {
+        onClick={toggleNavbar}
+        className={cn(styles.overlay, {
           [styles.active]: isOpen,
         })}
-      >
-        <p>Menu</p>
-        <ul>
-          {navItems.map(({ path, text }) => (
-            <li
-              key={text}
-              onClick={closeNavbar}
-              className={styles.menuListItem}
-            >
-              <Link to={path}>{text}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <button
-        className={styles.hamburgerButton}
-        aria-controls="navigation"
-        aria-expanded={isOpen}
-        onClick={() => setIsOpen((prevIsOpen) => !prevIsOpen)}
-      >
-        <span
-          className={cn(styles.hamburger, {
+      ></div>
+      <nav role="navigation" aria-label="menu" className={styles.navbar}>
+        <div
+          className={cn(styles.menuList, {
             [styles.active]: isOpen,
           })}
-        ></span>
-      </button>
-    </nav>
+        >
+          <p>Menu</p>
+          <ul>
+            {navItems.map(({ path, text }) => (
+              <li
+                key={text}
+                onClick={toggleNavbar}
+                className={styles.menuListItem}
+              >
+                <Link to={path}>{text}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button
+          className={styles.hamburgerButton}
+          aria-controls="navigation"
+          aria-expanded={isOpen}
+          onClick={toggleNavbar}
+        >
+          <span
+            className={cn(styles.hamburger, {
+              [styles.active]: isOpen,
+            })}
+          ></span>
+        </button>
+      </nav>
+    </>
   );
 };
