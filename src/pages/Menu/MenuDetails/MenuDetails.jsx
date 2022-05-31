d643d569b00a099bef05f875fc64184008f02162
Robert Banaszak <robert.banaszak1997@gmail.com>
2022-05-31 16:42:41 +0200

    menu details (#77)

--- src/pages/Menu/MenuDetails/MenuDetails.jsx
+++ src/pages/Menu/MenuDetails/MenuDetails.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 import useMenuDetails from "@/pages/Menu/hooks/api/useMenuDetails";
-import { Button } from "@/components";
+import { Button, ButtonGroup, Card, Text } from "@/components";
+
+function getTitleText(stringDate) {
+  const date = new Date(stringDate);
+  const dayMonth = date.toLocaleDateString("pl-PL", {
+    day: "2-digit",
+    month: "long",
+  });
+  const weekday = date.toLocaleDateString("pl-PL", { weekday: "short" });
+
+  return `${dayMonth} (${weekday})`;
+}
 
 function MenuDetails() {
   const { date } = useParams();
-  const { data } = useMenuDetails(date);
+  const { data = [] } = useMenuDetails(date);
 
   return (
     <>
-      <Link to="/menu">back to Menu</Link>
-      <p>MenuDetails</p>
-      <pre style={{ overflow: "scroll" }}>{JSON.stringify(data, null, 2)}</pre>
+      <Text size="h3">{getTitleText(date)}</Text>
+      {data.map(({ id, recipe, portions }) => (
+        <Card
+          key={id}
+          primaryText={recipe.name}
+          secondaryText={`Zaplanowane porcje - ${portions}`}
+        />
+      ))}
       <Button to={`/menu/${date}/add`}>Dodaj danie</Button>
+      <ButtonGroup>
+        <Button variant="secondary" fullwidth to="/menu">
+          Cofnij
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
